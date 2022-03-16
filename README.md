898908f5a8de3850740f9e0963bcc3e9e0a13ec3
mushding <ajy1005464@gmail.com>
2022-03-16 15:39:22 +0800

    complete crawler artical (add ip content column)

--- README.md
+++ README.md
@@ -4,7 +4,16 @@ PPT 關鍵字分析
 
 ### crawler_artical.py
 找文章列表
-['title', 'push', 'date', 'author', 'link']
+```
+aid       文章 id -> 網址 index + 文章順序
+push      討論推/噓熱門度 
+title     文章標題
+author    文章作者
+ip        五章作者發文 ip
+postTime  發文時間
+link      文章連結
+content   文章內容
+```
 
 ### crawler_content.py
 找內文
--- crawler_artical.py
+++ crawler_artical.py
@@ -3,6 +3,7 @@ from requests_html import HTML
 from bs4 import BeautifulSoup
 import urllib
 import re
+from tqdm import tqdm
 from six import u
 
 import csv
@@ -36,6 +37,7 @@ def parseMeta(entry, aid):
 
     # get artical link
     link = entry.select('.title a')[0].attrs['href']
+    article_id = re.sub('\.html', '', link.split('/')[-1])
     href = "https://www.ptt.cc{}".format(link)
     resp = fetchData(href)
     soup = BeautifulSoup(resp, 'html.parser')
@@ -43,13 +45,13 @@ def parseMeta(entry, aid):
 
 
     # get meta information (author, title, postTime)
-    metaLine = soup.select('div.article-metaline')
+    metaLine = soup.select('.article-metaline .article-meta-value')
     author = metaLine[0].text
     title = metaLine[1].text
     postTime = metaLine[2].text
     
     # remove meta nodes
-    for meta in metaLine:
+    for meta in soup.select('.article-metaline'):
         meta.extract()
     for meta in soup.select('.article-metaline-right'):
         meta.extract()
@@ -59,36 +61,49 @@ def parseMeta(entry, aid):
     for push in pushes:
         push.extract()
 
+    # get author ip address
     try:
         ip = soup.find(text=re.compile(u'※ 發信站:'))
         ip = re.search('[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
     except:
         ip = "None"
     
-    filtered = [ v for v in soup.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] not in [u'--'] ]
+    # ones remove the meta, push, ip, ..., use soup.stripped_strings to get all text in html
+    # and pass into text filter
+    filtered = [ v for v in soup.stripped_strings if v[0] not in [u'※', u'◆', ':', '^'] and v[:2] not in [u'--'] ]
     expr = re.compile(u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
     for i in range(len(filtered)):
         filtered[i] = re.sub(expr, '', filtered[i])
 
     filtered = [_f for _f in filtered if _f]  # remove empty strings
     filtered = [x for x in filtered if article_id not in x]  # remove last line containing the url of the article
+    filtered = [x for x in filtered if 'http' not in x]  # remove any href link
+    filtered = [x for x in filtered if 'Sent from' not in x]  # remove upload by phone
     content = ' '.join(filtered)
     content = re.sub(r'(\s)+', ' ', content)
-    print ('content:', content)
 
     meta = {
         'aid': aid,
-        'title': title,
         'push': entry.select('.nrec')[0].text,
-        'date': postTime,
+        'title': title,
         'author': author,
+        'ip': ip,
+        'postTime': postTime,
         'link': link,
+        'content': content
     }
     return meta
 
+def saveToCSV(pageData, fieldnames, saveDir):
+    with open(saveDir, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for data in pageData:
+            writer.writerow(data)
+
 if __name__ == "__main__":
     pageData = list()
-    pageNum = 100
+    pageNum = 1
 
     # Step 1. get Gossiping Data pass through over-18
     url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
@@ -97,37 +112,31 @@ if __name__ == "__main__":
     # convert rawData to HTML
     initHTML = HTML(html=resp)
 
-    # next page
+    # get next page
     btns = initHTML.find('.action-bar a.btn.wide')
     prevPageBtn = btns[1].attrs['href']
 
     # get prevPage index
     prevPageIdx = int(prevPageBtn.split('/')[3].replace('index', '').split('.')[0])
 
-    for pageIdx in range(prevPageIdx - pageNum, prevPageIdx + 1):
+    # Step 2. every link has 20 articles, from present to past `pageNum` links
+    for pageIdx in tqdm(range(prevPageIdx - pageNum, prevPageIdx + 1)):
         page_url = urllib.parse.urljoin('https://www.ptt.cc/bbs/Gossiping/', 'index' + str(pageIdx) + ".html")
-        print(pageIdx)
         resp = fetchData(page_url)
         soup = BeautifulSoup(resp, 'html.parser')
-        # Step 2. get the article block
+        # Step 3. into every article blocks
         post_entries = soup.select('.r-ent')
         for articleIdx, entry in enumerate(post_entries):
             aid = str(pageIdx) + str(articleIdx).zfill(2)
-            print(aid)
             meta = parseMeta(entry, aid)
             # if the article is deleted, then don't save it to list
             if meta == None:
                 print("article was deleted")
             else:
-                print(meta)
                 pageData.append(meta)
-    # csv header
-    fieldnames = ['aid', 'title', 'push', 'date', 'author', 'link']
-    with open('Gossiping_100.csv', 'w', encoding='UTF8', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        # writer.writerow(pageData)
-        for data in pageData:
-            writer.writerow(data)
-    
+
+    # Step 4. save as csv file
+    fieldnames = ['aid', 'push', 'title', 'author', 'ip', 'postTime', 'link', 'content']
+    saveDir = 'Gossiping_100.csv'
+    saveToCSV(pageData, fieldnames, saveDir)
     print("end of the code")
